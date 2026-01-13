d2a10ed6b213e46346603c3a2b227c7b6cade209
Nichita Danilov <danilovnichita@gmail.com>
2026-01-13 17:10:32 +0100

    computing relative monthly precipitation

--- analysis.py
+++ analysis.py
@@ -1,41 +1,54 @@
 import json
-location = {}
+stations = { "GHCND:USW00093814", "GHCND:US1WAKG0038", "GHCND:USC00513317", "GHCND:US1CASD0032"}
 monthly = 0
 list_dates = []
 total_precipitation = 0
 monthly_precipitation = {}
+all_stations_total_precipitation = 0
 sum = 0
 
 with open('precipitation.json') as file:
     data = json.load(file)
-for dat in data:
+    for total_rain in data:
+        all_stations_total_precipitation = all_stations_total_precipitation + int(total_rain["value"])
+    for station in stations: 
+        relative_yearly_precipitation = 0
+        total_precipitation = 0
+        for dat in data: 
+            if dat['station'] == station:
+                date = dat["date"]
+                dates = date.split("-")
+                months_index = dates[1]
+                total_precipitation = total_precipitation + dat['value']
+            
+                if months_index not in monthly_precipitation:
+                    monthly_precipitation[months_index] = 0
+                monthly_precipitation[dates[1]] += dat["value"]
+
+
+                text_list = []
+                text_list_relative = []
+                
+                for items in  monthly_precipitation :
+                    text = f'Monthly precipitation in month {items} is {monthly_precipitation[items]}'
+                    text_list.append(text)
+                    #text_relative = f'The relative precipitacion for month {items} is {monthly_precipitation[items]/total_precipitation}'
+                    #text_list_relative.append(text_relative)
+                
+            relative_yearly_precipitation = total_precipitation/all_stations_total_precipitation
+
         
-    if dat['station'] == 'GHCND:US1WAKG0038':
-        date = dat["date"]
-        dates = date.split("-")
-        months_index = dates[1]
-        total_precipitation = total_precipitation + int(dat['value'])
+        
+
+            
+        print(relative_yearly_precipitation)
     
-        if months_index not in monthly_precipitation:
-            monthly_precipitation[months_index] = 0
-        monthly_precipitation[dates[1]] += dat["value"]
-
-
-text_list = []
-text_list_relative = []
-for items in  monthly_precipitation :
-    text = f'Monthly precipitation in month {items} is {monthly_precipitation[items]}'
-    text_list.append(text)
-    text_relative = f'The relative precipitacion for month {items} is {monthly_precipitation[items]/total_precipitation}'
-    text_list_relative.append(text_relative)
-
-
-print(text_list_relative)
-#print(text_list)         
+        #print(all_stations_total_precipitation)
+        #print(relative_yearly_precipitation)         
           
 
-print(total_precipitation)
-with open ("results.json", "w", encoding = "utf-8") as file:
-       json.dump(text_list, file, indent = 4)
+#print(total_precipitation)
+#with open ("results.json", "w", encoding = "utf-8") as file:
+     #  json.dump(text_list, file, indent = 4)
 
    
